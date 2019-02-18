f19d123991827cc629a9dd60fbab1e1a781c5b05
a714614805 <42933976+a714614805@users.noreply.github.com>
2019-02-18 11:08:53 +0800

    try scroll-css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div id="kuai"></div>
     <router-view/>
   </div>
 </template>
@@ -19,5 +20,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#kuai {
+  height:5999px;
+  width:100%;
+}
+::-webkit-scrollbar {
+width:12px;
+}
+/* 滚动槽 */
+::-webkit-scrollbar-track {
+-webkit-box-shadow:inset006pxrgba(0,255,0,0.3);
+border-radius:10px;
+}
+::-webkit-scrollbar-track-piece{
+  -webkit-box-shadow:2px 2px 2px 2px;
+  background : rgba(0,0,255,0.5);
+}
+/* 滚动条滑块 */
+::-webkit-scrollbar-thumb {
+border-radius:10px;
+background:rgba(255, 0, 0, 1);
+-webkit-box-shadow:inset006pxrgba(0,0,0,0.5);
+}
+::-webkit-scrollbar-thumb:window-inactive {
+background:rgba(0, 0, 0, 0);
 }
 </style>
